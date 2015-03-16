9f6a18a82eed4d52d9e634c6a5fdbc1005902a65
Kevin Delfour <kde.dekkastud10@gmail.com>
2015-03-16 20:31:47 +0000

    Add others view

---
+++ modules_core/users/views/auth/_layout.php
@@ -0,0 +1,6 @@
+<div class="outside login-screen">
+    <?php echo $content; ?>
+    <div class="text text-center animated fadeIn">
+        Powered by <a href="http://www.humhub.org" target="_blank">HumHub</a>
+    </div>
+</div>
